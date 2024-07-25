dcef2ccefb737ec9404a5473e8d45890bc3d2e6b
Korada Sai Sampath Kumar <118899372+saisampath33@users.noreply.github.com>
2024-07-25 16:43:05 +0530

    Update Auth.jsx

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -16,7 +16,7 @@ function Auth() {
       // api request to login or register
 
       const { data } = await axios.post(
-        `http://localhost:3001/api/users${isRegister ? "" : "/login"}`,
+        `https://blogging-website-backend-1-j9ye.onrender.com/api/users${isRegister ? "" : "/login"}`,
         { user: values }
       );
 
