d2f56b239baa80ba1f729d0bda52d463a6d36c3e
Korada Sai Sampath Kumar <118899372+saisampath33@users.noreply.github.com>
2024-07-25 16:46:13 +0530

    Update useCreateArticle.js

--- src/hooks/useCreateArticle.js
+++ src/hooks/useCreateArticle.js
@@ -7,7 +7,7 @@ const creteArticle = async (values) => {
     console.log("creteArticle", { values });
 
     const { data } = await axios.post(
-        `http://localhost:3001/api/articles`,
+        `https://blogging-website-backend-1-j9ye.onrender.com/api/articles`,
         { article: {...values.values} }
       );
 
