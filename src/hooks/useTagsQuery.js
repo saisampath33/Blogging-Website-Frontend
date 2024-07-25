fc8a0a30ff874b784d44ec4c61af1edfabf1338b
Korada Sai Sampath Kumar <118899372+saisampath33@users.noreply.github.com>
2024-07-25 16:14:21 +0530

    Add files via upload
    This is frontend repo

---
+++ src/hooks/useTagsQuery.js
@@ -0,0 +1,30 @@
+
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import axios from 'axios';
+
+const getAllTags = async () => {
+  const {data} = await axios.get(`http://localhost:3001/api/tags`);
+  return data;
+};
+
+function useTagsQuery() {
+  const {
+    isLoading: isTagsLoading,
+    data: tags,
+    error: tagsError,
+  } = useQuery({
+    queryKey: ["tags"],
+    queryFn: getAllTags,
+    refetchOnWindowFocus: true,
+    staleTime: 0,
+    cacheTime: 0,
+  });
+  return {
+    isTagsLoading,
+    tags,
+    tagsError,
+  };
+}
+
+export default useTagsQuery;
