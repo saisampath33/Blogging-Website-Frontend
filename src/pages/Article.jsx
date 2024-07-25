aebfdcdd4022cbac6870376ffc38eafdfdc78948
Korada Sai Sampath Kumar <118899372+saisampath33@users.noreply.github.com>
2024-07-25 16:42:22 +0530

    Update Article.jsx
    changed the link

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -12,7 +12,7 @@ function Article() {
     console.log('article', article)
 
     const getArticleBySlug = async (slug) => {
-        const { data } = await axios.get(`http://localhost:3001/api/articles/${slug}`);
+        const { data } = await axios.get(`https://blogging-website-backend-1-j9ye.onrender.com/api/articles/${slug}`);
 
         console.log("getArticleBySlug", { data });
 
